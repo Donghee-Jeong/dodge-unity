bb350ada9d34482d9995bbb7cfaf70072b72dfd6
Donghee-Jeong <weg9511@gmail.com>
2023-12-12 22:29:29 +0900

    playerRigidBody 코드 수준에서 적용 및 플레이어의 이동 개선(velocity)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,35 +6,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
     public float speed = 8f;
 
     private void Start()
     {
-        throw new NotImplementedException();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, - speed);
-        }
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
 
-        if (Input.GetKey((KeyCode.RightArrow)) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
     }
     
     public void Die()

3fbbfba601f4e784145baa92f514f27700aa472b
Donghee-Jeong <weg9511@gmail.com>
2023-12-12 23:11:24 +0900

    총알 생성기 생성

---
+++ Assets/BulletSpawner.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterSpawn;
+
+    private void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    private void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if (timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
+    }
+}
